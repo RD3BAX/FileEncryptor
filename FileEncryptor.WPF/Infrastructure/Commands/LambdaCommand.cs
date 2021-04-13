cc183402489c70345e7ef4970d8091adbc46b4d6
Denis Novikov <rd3bax@gmail.com>
2021-04-13 19:41:59 +0300

    Команды

---
+++ FileEncryptor.WPF/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileEncryptor.WPF.Infrastructure.Commands.Base;
+
+namespace FileEncryptor.WPF.Infrastructure.Commands
+{
+    class LambdaCommand : Command
+    {
+        #region Поля
+
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        #endregion // Поля
+
+        #region Методы
+
+        protected override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        protected override void Execute(object parameter) => _execute(parameter);
+
+        #endregion // Методы
+
+        #region Конструктор
+
+        public LambdaCommand(Action Execute, Func<bool> CanExecute = null) 
+            : this(
+                Execute: p => Execute(), 
+                CanExecute: CanExecute is null ? (Func<object, bool>)null : p => CanExecute())
+        {
+            
+        }
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        #endregion // Конструктор
+    }
+}
