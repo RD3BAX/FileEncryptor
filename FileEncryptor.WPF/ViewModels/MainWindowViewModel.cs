5195ece7faaaee936629405f6d4e092a5a07c48c
Denis Novikov <rd3bax@gmail.com>
2021-04-13 04:10:30 +0300

    Модель-представления

---
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FileEncryptor.WPF.ViewModels.Base;
+
+
+namespace FileEncryptor.WPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Свойства
+
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string _Title;
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion // Заголовок окна
+
+        #endregion // Свойства
+    }
+}
