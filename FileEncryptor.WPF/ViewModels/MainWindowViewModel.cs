0b964894cbf338890e86a18e8fd2156285de4a8f
Denis Novikov <rd3bax@gmail.com>
2021-04-14 19:00:31 +0300

    Выбор файла

--- FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
+++ FileEncryptor.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,20 @@
-﻿using FileEncryptor.WPF.ViewModels.Base;
+﻿using System.IO;
+using System.Windows.Input;
+using FileEncryptor.WPF.Infrastructure.Commands;
+using FileEncryptor.WPF.Services.Interfaces;
+using FileEncryptor.WPF.ViewModels.Base;
 
 
 namespace FileEncryptor.WPF.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Поля
+
+        private readonly IUserDialog _UserDialog;
+
+        #endregion // Поля
+
         #region Свойства
 
         #region Title : string - Заголовок окна
@@ -21,12 +31,67 @@ namespace FileEncryptor.WPF.ViewModels
 
         #endregion // Заголовок окна
 
+        #region Password : string - Пароль
+
+        /// <summary>Пароль</summary>
+        private string _Password;
+
+        /// <summary>Пароль</summary>
+        public string Password
+        {
+            get => _Password;
+            set => Set(ref _Password, value);
+        }
+
+        #endregion // Пароль
+
+        #region SelectedFile : FileInfo - Выбранный файл
+
+        /// <summary>Выбранный файл</summary>
+        private FileInfo _SelectedFile;
+
+        /// <summary>Выбранный файл</summary>
+        public FileInfo SelectedFile
+        {
+            get => _SelectedFile;
+            set => Set(ref _SelectedFile, value);
+        }
+
+        #endregion // Выбранный файл
+
         #endregion // Свойства
+
+        #region Команды
+
+        #region Command : SelectFileCommand - Выбрать файл
+
+        private ICommand _SelectFileCommand;
+
+        /// <summary>Выбрать файл</summary>
+        public ICommand SelectFileCommand => _SelectFileCommand
+            ??= new LambdaCommand(OnSelectFileCommandExecuted/*, CanSelectFileCommandExecute*/);
+
+        ///// <summary>Проверка возможности выполнения - Выбрать файл</summary>
+        //private bool CanSelectFileCommandExecute(object p) => true;
+
+        /// <summary>Логика выполнения - Выбрать файл</summary>
+        private void OnSelectFileCommandExecuted(object p)
+        {
+            if (!_UserDialog.OpenFile("Выбор файла для шифрования", out var file_path)) return;
+            var selected_file = new FileInfo(file_path);
+            SelectedFile = selected_file.Exists ? selected_file : null;
+        }
+
+        #endregion // SelectFileCommand
+
+
+        #endregion // Команды
 
         #region Конструктор
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IUserDialog UserDialog)
         {
+            _UserDialog = UserDialog;
             Title = "Шифратор";
         }
 
